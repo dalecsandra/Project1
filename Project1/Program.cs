dd3b50d76d2f2a3192242b0277d33ca3bc5c759a
Administrator <Administrator@HOMEPC>
2022-11-27 22:45:18 +0200

    Add project files.

---
+++ Project1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Project 1 Alexandra!");
+        }
+    }
+}
